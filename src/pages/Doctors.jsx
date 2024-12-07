34ff16c2760134fa30b1dbf6a34622a9a06d544f
Atul Mishra <atulkmishra858@gmail.com>
2024-12-07 20:47:49 +0530

    change the filter option in Ui

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -32,7 +32,7 @@ function Doctors() {
       <div className='flex-col mt-3'>
       <strong className="text-lg font-semibold">Browse through the doctors speciality</strong>
       <br />
-      <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter?'bg-primary text-white':''}`} onClick={()=>setShowFilter(prev=>!prev)}>Filter</button>
+      <button className={`py-1 px-3 border rounded text-sm transition-all  ${showFilter?'bg-primary text-white':''}`} onClick={()=>setShowFilter(prev=>!prev)}>Filter</button>
       <div className={` ${showFilter?'block':' hidden sm:block'}border-[2px] rounded-lg border-blue-400 outline-double mt-2`}>
         <h3 onClick={()=>speciality==='Genenral physician'?navigate('/doctors'):navigate('/doctors/General physician')}  className='p-2 border border-blue-300  mt-2 rounded-lg bg-blue-200 text-gray-700 hover:bg-blue-300 transition-all duration-500'>Genearl physician</h3>
         <h3 onClick={()=>speciality='Gynecologist'?navigate('/doctors'):navigate('/doctors/Gynecologist')} className='p-2 border border-blue-300  mt-2 rounded-lg bg-blue-200 text-gray-700 hover:bg-blue-300 transition-all duration-500'>Gynecologist</h3>
